bddb4f3ecfd2628344e79dc846ae410b9de7a5bb
Suyash Singh <suyashsingh234@gmail.com>
2020-04-27 23:13:35 +0530

    navbar: made world map button visible

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,14 @@ function Navbar () {
     <div>
       <ReactBootstrap.Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <ReactBootstrap.Navbar.Brand><Link style={{ textDecoration: 'inherit', color: 'inherit' }} to='/'>India's_Covi19_Tracker</Link></ReactBootstrap.Navbar.Brand>
+<ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/country'>World Map</Link></ReactBootstrap.Nav.Link>
         <ReactBootstrap.Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <ReactBootstrap.Navbar.Collapse id='responsive-navbar-nav'>
-          <ReactBootstrap.Nav className='mr-auto'>
+          <ReactBootstrap.Nav className="ml-auto">
             <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/countryStats'>World Stats</Link></ReactBootstrap.Nav.Link>
             <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/precaution'>Precautions</Link></ReactBootstrap.Nav.Link>
             <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/symptoms'>Symptoms</Link></ReactBootstrap.Nav.Link>
             <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/app'>Aarogya Setu</Link></ReactBootstrap.Nav.Link>
-          </ReactBootstrap.Nav>
-          <ReactBootstrap.Nav>
-            <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/country'>World Map</Link></ReactBootstrap.Nav.Link>
             <ReactBootstrap.Nav.Link><Link style={{ textDecoration: 'inherit', color: 'inherit' }} className='lessblue' to='/news'>News</Link></ReactBootstrap.Nav.Link>
           </ReactBootstrap.Nav>
         </ReactBootstrap.Navbar.Collapse>
